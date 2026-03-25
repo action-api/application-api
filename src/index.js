2c606963cd123a4fcc2732f35521ff7426dcdeed
MadakaHeri <madakaheri@icloud.com>
2026-03-25 16:21:47 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {Auth} from 'utils/auth/index.js';
 import {router} from './utils/router/index.js';
 import * as config from './config.js';
 
@@ -7,6 +8,9 @@ import * as config from './config.js';
 export async function handler(event) {
 	const {httpMethod, path, body, requestContext} = event;
 	const {cognitoAuthenticationProvider, cognitoIdentityId, sourceIp} = requestContext.identity;
+
+	const auth = new Auth(event);
+	await auth.fetchCurrentAuthenticationUser();
 
 	const response = await router(event, config);
 
